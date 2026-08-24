81a9900d2331c86b5d56ad9612d1e587858896f8
Ivan Goncharov <indecor55@gmail.com>
2026-08-24 14:26:53 +0700

    refactor: extract shared JSON response handling

--- src/main/ru/practicum/moviehub/http/MoviesHandler.java
+++ src/main/ru/practicum/moviehub/http/MoviesHandler.java
@@ -3,21 +3,11 @@ package ru.practicum.moviehub.http;
 import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
 
 public class MoviesHandler extends BaseHttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String responseBody = "[]";
-        byte[] responseBytes = responseBody.getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders()
-                .set("Content-Type", "application/json; charset=UTF-8");
-        exchange.sendResponseHeaders(200, responseBytes.length);
-
-        try (OutputStream outputStream = exchange.getResponseBody()) {
-            outputStream.write(responseBytes);
-        }
+        sendJson(exchange, 200, "[]");
     }
 }
